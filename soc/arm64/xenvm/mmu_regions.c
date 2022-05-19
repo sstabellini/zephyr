7df99a12ab870b59e0e4af7b730d7b5d3fb43945
Stefano Stabellini <stefano.stabellini@xilinx.com>
2022-05-19 12:16:11 -0700

    Add a shared memory and event channels interface
    Include the xen.h header file with useful Xen hypercall definitions.
    The original location is:
    https://xilinx-wiki.atlassian.net/wiki/spaces/A/pages/2251030537/Xen+Shared+Memory+and+Interrupts+Between+VMs#Example
    
    Add a shared memory interface at address 0x7fe01000.
    
    Bind a Xen event channel and write the number to the shared memory
    location. For each new TTC timer event write the timer counter value to
    the shared memory for the other domain to read. Signal data readiness
    by sending an event channel notification.
    
    Signed-off-by: Stefano Stabellini <stefano.stabellini@xilinx.com>

--- soc/arm64/xenvm/mmu_regions.c
+++ soc/arm64/xenvm/mmu_regions.c
@@ -23,6 +23,11 @@ static const struct arm_mmu_region mmu_regions[] = {
 			      DT_REG_ADDR_BY_IDX(DT_INST(0, xlnx_ttcps), 0),
 			      DT_REG_SIZE_BY_IDX(DT_INST(0, xlnx_ttcps), 0),
 			      MT_DEVICE_nGnRnE | MT_P_RW_U_RW | MT_NS),
+
+	MMU_REGION_FLAT_ENTRY("RAM_SHARED",
+			      0x7fe01000,
+			      0x1000,
+			      MT_NORMAL_NC | MT_P_RW_U_RW | MT_NS),
 };
 
 const struct arm_mmu_config mmu_config = {
